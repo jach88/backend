1573538577d1b7349d15c6aaa58de3e1f47be196
jach88 <33399125+jach88@users.noreply.github.com>
2021-08-16 21:50:43 -0500

    firts commit

---
+++ Dia1_1608/01-variables.py
@@ -0,0 +1,22 @@
+#Esto es un comentario
+
+#variables de texto
+nombre = "eduardo" 
+apellido = 'de rivero'
+
+direccion = "calle o'higgins 125"  #si en el texto existe comilla simple se usa la doble y viceversa
+
+texto = """Hola Amigos:
+buenas noches, les saluda su profe 
+atentamente,
+ eduardo """   #triple comilla cuando se quiere guardar el texto con el formato
+
+texto2 = '''Hola amigos:
+ ya casi viene el texto'''
+
+#variables numericas
+año = 2021
+estatura = 1.92
+
+print(type(nombre))
+print(type(año))
