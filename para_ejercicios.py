187119eec9deb7a877cb4853f19a1921e7e5b060
Christian Julca <jach88@hotmail.com>
2021-08-19 10:04:33 -0500

    Ejercicio propuesto clase3

--- para_ejercicios.py
+++ para_ejercicios.py
@@ -1,10 +1,28 @@
-numeros =[1,2,5,9,12,15,17,19,21,39]
-for numero in numeros:
-    if(numero%3==0 and numero%5==0):
-        print(f"{numero} es multiplo de 3 y de 5")
-    elif(numero%3==0):
-        print(f"{numero} es multiplo de 3")
-    elif(numero%5==0):
-        print(f"{numero} es multiplo de 5")
-   
-    
+import random
+
+intentos = 0
+numeroIntentos = 6
+NumeroMin = 1
+NumeroMax = 20
+
+numeroAdivinar = random.randint(NumeroMin,NumeroMax)
+print(f"Hola intenta adivinar un numero entre {NumeroMin} y {NumeroMax}")
+
+while intentos < numeroIntentos:
+    print("Indicame un numero: ")
+    numero = input()
+    numero = int(numero)
+
+    intentos = intentos + 1
+
+    if numero < numeroAdivinar:
+        print("Tu numero es menor")
+    if numero > numeroAdivinar:
+        print("Tu numero es mayor")
+    if numero == numeroAdivinar:
+        break
+
+if numero == numeroAdivinar:
+    print("Excelente has adivinado el numero")
+if numero != numeroAdivinar:
+    print(f"Lo siento el numero era {numeroAdivinar} intentalo nuevamente")
