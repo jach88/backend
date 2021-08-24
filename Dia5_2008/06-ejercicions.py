bf406ed01c6eb1654255a6e936ee0e959c82533a
Christian Julca <jach88@hotmail.com>
2021-08-23 19:23:19 -0500

    Limpieza de la rama

--- Dia5_2008/06-ejercicions.py
+++
@@ -1,6 +0,0 @@
-#ejemplo
-#para evitar que en cadad impresion se ejecute una nueva linea, se puede agregar el parametro end y este indicara como queremos que actue al finalizar la linea
-
-for numero in range(5):
-    print(numero,end="")
-
