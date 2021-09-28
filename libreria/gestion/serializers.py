ee48ab419590c0b524484ebcc94b63931023903a
Christian Julca <jach88@hotmail.com>
2021-09-27 22:09:28 -0500

    Chore clase Semana7 dia1

--- libreria/gestion/serializers.py
+++ libreria/gestion/serializers.py
@@ -1,5 +1,9 @@
+from re import M
+from typing import ClassVar
+from django.db.models import fields
+import requests
 from rest_framework import serializers
-from .models import ProductoModel
+from .models import CabeceraModel, ClienteModel, DetalleModel, ProductoModel
 
 class ProductoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,3 +19,48 @@ class ProductoSerializer(serializers.ModelSerializer):
         # exclude = ['productoId']
 
         #no se puede utilizar los dos atributos al mismo tiempo, es decir, o usamos el exclude o usamos el fields
+
+class ClienteSerializer(serializers.ModelSerializer):
+    clienteNombre = serializers.CharField(
+        max_length=45, required=False, trim_whitespace=True, read_only=True)
+    clienteDireccion = serializers.CharField(
+        max_length=200, required=False, trim_whitespace= True)
+
+    class Meta:
+        model = ClienteModel
+        fields = '__all__'
+
+class DetalleOperacionSerializer(serializers.Serializer):
+    cantidad = serializers.IntegerField(required=True, min_value=1)
+
+    importe = serializers.DecimalField(max_digits=5, decimal_places=2, min_value=0.01, required=True)
+
+    producto = serializers.IntegerField(required=True, min_value=1)  
+
+class OperacionSerializer(serializers.Serializer):
+    tipo = serializers.ChoiceField(
+        choices=[('V','VENTA'),('C','COMPRA')], required=True)
+
+    cliente = serializers.IntegerField(required= True,min_value=1)
+
+    detalle= DetalleOperacionSerializer(many=True)
+
+class DetalleOperacionModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleModel
+        # fields = '__all__'
+        exclude = ['cabeceras']
+        depth = 1
+
+
+class OperacionModelSerializer(serializers.ModelSerializer):
+    cabeceraDetalles = DetalleOperacionModelSerializer(
+        # source='cabeceraDetalles',
+        many=True)
+
+    class Meta:
+        model = CabeceraModel
+        fields = '__all__'
+        #con el atributo deph indicare cuantos niveles quiero agregar para mostrar la informacion en el caso de las FKs
+        depth = 1
+
--- libreria/gestion/views.py
+++ libreria/gestion/views.py
@@ -1,41 +1,55 @@
 from django.core.exceptions import ImproperlyConfigured
+from django.db.models.query import QuerySet
+from django.db.models.query_utils import subclasses
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .models import ProductoModel
-from .serializers import ProductoSerializer
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import CabeceraModel, DetalleModel, ProductoModel, ClienteModel
+from .serializers import( ClienteSerializer, OperacionSerializer, ProductoSerializer,
+                            OperacionModelSerializer)
 from rest_framework import status
+from .utils import PaginacionPersonalizada
+import requests as solicitudes
+from os import environ
+from django.db import transaction, Error
+from datetime import datetime
+from django.shortcuts import get_object_or_404, get_list_or_404
+
 
 class PruebaController(APIView):
     def get(self, request, format=None):
         return Response(data={'message':'Exito'}, status=200)
 
     def post(self, request:Request, format=None):
-        print(request.data)
+        # print(request.data)
         return Response(data={'message':'Hiciste post'})
+
 
 class ProductosController(ListCreateAPIView):
     #pondremos la consulta de ese modelo en la bd
     queryset = ProductoModel.objects.all()  #select *from productos;
     serializer_class = ProductoSerializer
+    pagination_class = PaginacionPersonalizada
 
-    def get(self, request):
-        respuesta = self.get_queryset().filter(productoEstado=True).all()
-        print(respuesta)
-        #instance=> cuando tenemos informacion en la bd y la queremos serializar para mostrarla al cliente
-        #data => para ver si la ifnroamcion que me esta enviando el cliente esta buena o no
-        #many=true => sirve para indicar que estamos pasando una lista de instancias de la clase del modelo
-        respuesta_serializada = self.serializer_class(
-            instance=respuesta, many=True)
-        return Response(data={
-            "message": None,
-            "content":respuesta_serializada.data
+    # def get(self, request):
+    #     respuesta = self.get_queryset().filter(productoEstado=True).all()
+    #     print(respuesta)
+    #     #instance=> cuando tenemos informacion en la bd y la queremos serializar para mostrarla al cliente
+    #     #data => para ver si la ifnroamcion que me esta enviando el cliente esta buena o no
+    #     #many=true => sirve para indicar que estamos pasando una lista de instancias de la clase del modelo
+    #     respuesta_serializada = self.serializer_class(
+    #         instance=respuesta, many=True)
+    #     return Response(data={
+    #         "message": None,
+    #         "content":respuesta_serializada.data
 
-        })
+    #     })
     
     def post(self, request:Request):
-        print(request.data)
+        # print(request.data)
         data = self.serializer_class(data=request.data)
         # raise_excepcion lanzará la excepcion con el mensaje que dio el error y no permitirá continuar con el codigo siguiente
         if data.is_valid():
@@ -51,6 +65,7 @@ class ProductosController(ListCreateAPIView):
                 "message":"Error al guardar el producto",
                 "content":data.errors
             },status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProductoController(APIView):
    
@@ -126,4 +141,182 @@ class ProductoController(APIView):
         return Response(data={
             "message":"Producto eliminado exitosamente",
             "content":serializador.data
+        })
+
+
+class ClienteController(CreateAPIView):
+    queryset=ClienteModel.objects.all()
+    serializer_class = ClienteSerializer
+
+    # def get(self,request):
+    #     pass
+
+    def post(self, request: Request):
+        
+        data: Serializer = self.get_serializer(data=request.data)
+        if data.is_valid():
+            # print(data.validated_data)
+            # print(data.initial_data)
+            # print(data.data)
+            documento= data.validated_data.get('clienteDocumento')
+            direccion= data.validated_data.get('clienteDireccion')
+            url='https://apiperu.dev/api/'
+            if len(documento) == 8:
+                if direccion is None:
+                    return Response(data={
+                        'message':'Los clientes con DNI se debe proveer la direccion'
+                    }, status=status.HTTP_400_BAD_REQUEST)
+                url += 'dni/'
+            elif len(documento) == 11:
+                url += 'ruc/'
+
+            resultado = solicitudes.get(url+documento, headers={
+                'Content-Type':'application/json',
+                'Authorization':'Bearer '+environ.get('APIPERU_TOKEN')
+            }
+             
+            )
+            # print(resultado.json())
+            success =resultado.json().get('success')
+
+            #validar si el dni existe o no
+            if success is False:                
+                return Response(data={
+                    'message':'Documento incorrecto'
+                },status=status.HTTP_400_BAD_REQUEST)
+            
+            data= resultado.json().get('data')
+            nombre=data.get('nombre_completo') if data.get(
+                'nombre_completo') else data.get('nombre_o_razon_social')
+           #hacer algo similar con la direccion
+            direccion = direccion if len(
+                documento) == 8 else data.get('direccion_completa')
+
+            #guardado del cliente en la bd
+            nuevoCliente = ClienteModel(
+                clienteNombre=nombre, clienteDocumento=documento, clienteDireccion=direccion)
+            
+            nuevoCliente.save()
+
+            nuevoClienteSerializado: Serializer = self.serializer_class(
+                instance=nuevoCliente)
+
+            return Response(data={
+                'message':'Cliente agregado exitosamente',
+                'content': nuevoClienteSerializado.data
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data={
+                'message':'Error al ingresar el cliente',
+                'content': data.errors
+            },status=status.HTTP_400_BAD_REQUEST)
+
+
+class BuscadorClienteController(RetrieveAPIView):
+    serializer_class = ClienteSerializer
+
+    def get(self, request: Request):
+        print(request.query_params)
+        #primero validar si me esta pasando el nombre o el documento
+        nombre = request.query_params.get('nombre')
+        documento =request.query_params.get('documento')
+
+        #si tengo el documento hare una busqueda todos los clientes por ese documento
+        clienteEncontrado = None
+        if documento:
+            clienteEncontrado:  QuerySet = ClienteModel.objects.filter(
+                clienteDocumento=documento)
+            
+
+            data = self.serializer_class(instance=clienteEncontrado, many=True)
+
+            # return Response({'content':data.data})
+        
+        if nombre:
+
+
+            if clienteEncontrado is not None:
+                clienteEncontrado = clienteEncontrado.filter(
+                    clienteNombre__icontains=nombre).all()
+            
+            else:
+                clienteEncontrado = ClienteModel.objects.filter(
+                    clienteNombre__icontains=nombre).all()
+                
+
+        data = self.serializer_class(instance=clienteEncontrado, many=True)
+            #agregar los test para el cliente controller y su busqueda
+            #dar la opcion que se pueda
+
+        return Response(data={
+            'message':'Los usuarios son:',
+            'content':data.data
+        })
+            
+
+class OperacionController(CreateAPIView):
+    serializer_class = OperacionSerializer
+    def post(self, request: Request):
+        data = self.serializer_class(data=request.data)
+        if data.is_valid():
+            #Si el cliente existe o no por su documento
+            documento=data.validated_data.get('cliente')
+            clienteEncontrado = ClienteModel.objects.filter(
+                clienteDocumento = documento).first()
+            print(clienteEncontrado)
+            detalles = data.validated_data.get('detalle')
+            tipo = data.validated_data.get('tipo')
+
+            try:
+                with transaction.atomic():
+                    if clienteEncontrado is None:
+                        raise Error('Usuario no existe')
+                    nuevaCabecera = CabeceraModel(
+                        cabeceraTipo=tipo, clientes=clienteEncontrado)
+                    nuevaCabecera.save()
+
+                    for detalle in detalles:
+                        producto = ProductoModel.objects.get(
+                            productoId =detalle.get('producto'))
+
+                        DetalleModel(
+                            detalleCantidad = detalle.get('cantidad'),
+                            detalleImporte = producto.productoPrecio *
+                            detalle.get('cantidad'),
+                            productos=producto,
+                            cabeceras=nuevaCabecera).save()
+
+            except Error as e:
+                print(e)
+                return Response(data={
+                    'message': 'Error al crear la operacion',
+                    'content': e.args
+                })
+            except Exception as exc:
+                return Response(data={
+                    'message' : 'Error al crear la operacion',
+                    'content': exc.args
+                })
+                
+            return Response(data={
+                'message':'Operacion registrada exitosamente'
+            })
+        else:
+            return Response(data={
+                'message':'Error al crear la operacion',
+                'content': data.errors
+            },status=status.HTTP_400_BAD_REQUEST)
+
+
+class OperacionesController(RetrieveAPIView):
+    serializer_class = OperacionModelSerializer
+
+    def get(self, request: Request, id):
+        cabecera = get_object_or_404(CabeceraModel,pk=id)
+        #cabecera = cabeceraModel.objects.get(cabeceraId=id)
+        print(cabecera)
+        cabecera_serializada = self.serializer_class(instance=cabecera)
+        return Response(data={
+            'message': 'La operacion es:',
+            'Content': cabecera_serializada.data
         })
