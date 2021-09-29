3f7eb70948e4cf7b0a769bf0e9d5db444cf76d24
Christian Julca <jach88@hotmail.com>
2021-09-28 21:26:19 -0500

    chore:Agregando el host al settings

--- libreria/settings.py
+++ libreria/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-4!11*@+6ew$ipgf)d(&ro3r)@7ar86v27r$od@xsm4s)ld)-%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['libreria-django-christian.herokuapp.com']
 
 
 # Application definition
