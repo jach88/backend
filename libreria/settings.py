990fa4d652a44299acb1f98ffe025fd450a74e78
Christian Julca <jach88@hotmail.com>
2021-09-28 21:38:59 -0500

    chore:Agregue git noise

--- libreria/settings.py
+++ libreria/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ''
 ]
 
 ROOT_URLCONF = 'libreria.urls'
--- libreria/urls.py
+++ libreria/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('gestion/', include('gestion.urls'))
 ]
