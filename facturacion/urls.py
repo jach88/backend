2a7527a1e1e8135553bfec9c2d6bd395982de0db
Christian Julca <jach88@hotmail.com>
2021-10-05 19:41:28 -0500

    Chore: Limpieza de la rama

--- facturacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from .views import ComprobanteController
-
-urlpatterns = [
-    path('generar-comprobante/', ComprobanteController.as_view()) 
-]
