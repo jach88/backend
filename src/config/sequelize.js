b7389121a908d3dc0ebd2eef3f9a5107e715d6c5
Christian Julca <jach88@hotmail.com>
2021-10-12 19:18:24 -0500

    Chore: Se agregò base de datos

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,4 +1,5 @@
 import {Sequelize} from 'sequelize'
+require("dotenv").config();
 
-export const conexion = new Sequelize("postgresql://postgres:root@localhost:5432/tareas",
+export const conexion = new Sequelize(process.env.DATABASE_URL,
 {logging:false});
